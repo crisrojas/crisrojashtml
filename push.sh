cc1dc7a37b8f372ca9cd7aa547ad7fcd294a427b
Cristian Rojas <cristian.rojas@live.fr>
2020-05-20 19:22:16 +0200

    made changes

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+###########################
+# add all added/modified files
+git add .
+# commit changes
+git commit -am "made changes"
+# push to git remote repository
+git push -u origin master
+###########################
+echo Press Enter...
+read
